d2d762272aea5c4e771ebe3404765007ad82ffcc
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-14 11:37:05 +0530

    feat(scripts,upload-specs): add tarring step

--- scripts/upload-specs.ts
+++ scripts/upload-specs.ts
@@ -1,27 +1,43 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getStorage } from "firebase-admin/storage";
 import dotenv from "dotenv";
+import tar from "tar";
 
-dotenv.config();
-
-initializeApp({
-  credential: cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: (process.env.FIREBASE_PRIVATE_KEY as string).replace(
-      /\\n/g,
-      "\n"
-    ),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  }),
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-});
-
-const bucket = getStorage().bucket();
-
-bucket
-  .upload("./specs.tar", {
-    public: true,
-  })
+tar
+  .c(
+    {
+      file: "specs.tar",
+    },
+    ["./specs"]
+  )
   .then(() => {
-    console.log("Uploaded");
+    dotenv.config();
+
+    initializeApp({
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKey: (process.env.FIREBASE_PRIVATE_KEY as string).replace(
+          /\\n/g,
+          "\n"
+        ),
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      }),
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    });
+
+    const bucket = getStorage().bucket();
+
+    bucket
+      .upload("./specs.tar", {
+        public: true,
+      })
+      .then(() => {
+        console.log("Uploaded");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  })
+  .catch((err) => {
+    console.error(err);
   });
